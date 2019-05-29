e4f49454bb05254afae88b0c129433ab713ddf2d
Ahmed fathi <php.ahmedfathi@gmail.com>
2019-05-29 13:13:40 +0200

    Update laravel-sentiment.php

--- src/config/laravel-sentiment.php
+++ src/config/laravel-sentiment.php
@@ -1,30 +1,20 @@
 <?php
 return [
-    /**
-     * Min length of a token for it to be taken into consideration
-     */
-    'min_token_length' => 1,
-    /**
-     * Max length of a token for it to be taken into consideration
-     */
-    'max_token_length' => 15,
-    /**
-     * set the base folder for the data models
-     */
+    
     'types' => [
-        'positive' => 'pos',
-        'negative' => 'neg',
-        'neutral' => 'neu',
-        'question' => 'que'
+        'positive' => 'positive',
+        'negative' => 'negative',
+        'neutral' => 'neutral',
+        'question' => 'question'
     ],
-    'data_folder' => storage_path("app/sentiment/ar"),
-    'data_files' => [
-        'positive' => "sentiment/ar/data.pos.php",
-        'negative' => "sentiment/ar/data.neg.php",
-        'neutral' => "sentiment/ar/data.neu.php",
-        'question' => "sentiment/ar/data.que.php",
-        'prefix' => "sentiment/ar/data.prefix.php",
-        'ignore' => "sentiment/ar/data.ign.php"
+    'data_folder'      => storage_path("app/sentiment/ar"),
+    'data_files'       => [
+        'positive' => "sentiment/ar/data.positive.php",
+        'negative' => "sentiment/ar/data.negative.php",
+        'neutral'  => "sentiment/ar/data.neutral.php",
+        'question' => "sentiment/ar/data.question.php",
+        'prefix'   => "sentiment/ar/data.prefix.php",
+        'ignore'   => "sentiment/ar/data.ignore.php"
     ],
 
 ];
