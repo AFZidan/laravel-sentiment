5d27f82a8c7c45cdb4555e18326e0aff5438efce
Ahmed fathi <php.ahmedfathi@gmail.com>
2019-06-17 13:32:32 +0200

    Update Analise.php
    issues fixed

--- src/Analise.php
+++ src/Analise.php
@@ -72,7 +72,7 @@ class Analise extends Sentiment
 
     protected function clearifyScores($scores)
     {
-        if (array_count_values($scores) > 0) {
+        if (array_sum(array_values($scores)) > 0) {
             // the text is negative if positive and negative scores are equalitive
             if (( $scores['positive'] == $scores['negative'] ) && $scores['negative'] >= $scores['question']) {
                 $scores['negative'] = $scores['positive'] + 0.1;
